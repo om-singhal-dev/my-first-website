99c4053a1f1a3b4f0f6d0a14ab2746b93d6e9655
Om Singhal <omsinghal1401@gmail.com>
2026-08-10 15:46:36 +0530

    Practice JavaScript Loops & Nested Loops

---
+++ loops.js
@@ -0,0 +1,41 @@
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+let numbers1 = [5, 10, 15, 20];
+
+for (let i = 0; i < numbers1.length; i++) {
+  console.log(numbers1[i]);
+}
+
+
+let numbers2 = [2, 4, 6, 8];
+let total = 0;
+
+for (let i = 0; i < numbers2.length; i++) {
+  total = total + numbers2[i];
+}
+
+console.log(total);
+
+
+
+let numbers3 = [3, 8, 11, 14, 17, 20];
+
+for (let i = 0; i < numbers3.length; i++) {
+  if (numbers3[i] % 2 === 0) {
+    console.log(numbers3[i]);
+  }
+}
+
+
+
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
+
+let numbers4 = [100, 200, 300];
+
+for (let i = 0; i < numbers4.length; i++) {
+  console.log(numbers4[i]);
+}
